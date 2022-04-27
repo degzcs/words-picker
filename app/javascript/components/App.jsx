8fd682827f80f3320bbbb881b84da18a2d341f97
Diego Gomez <diego.gomez@encora.com>
2022-04-27 10:07:41 -0500

    select words function and add inputs

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,13 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "../routes/Index";
 
 const App = () => {
+  const [sentence, setSentence] = useState('');
+  const [selectedWords, setSelectedWords] = useState([]);
+
+  useEffect(() => {
+    setSentence('Apple is looking at buying U.K. startup for $1 billion.')
+    }, [])
+
+  const handleClick = (word) => {
+    setSelectedWords([...selectedWords, word]);
+  }
 
   return (
     <>
-    <br/>
-    <br/>
-     Hello Propoler
+      <div
+        className="container">
+        <div
+          className="alert alert-success"
+          role="alert">
+            {
+              sentence.split(' ').map((word, index) => {
+                return <span key={index} onClick={() => handleClick(word)}>
+                  {word}&nbsp;
+                </span>
+              })
+            }
+        </div>
+        <div
+          className="success">
+            {
+              selectedWords.map((word, index) => {
+                return <span key={index}>
+                  {word}&nbsp;
+                </span>
+              })
+            }
+          <input type='text'/>
+        </div>
+        <div
+          className="success">
+            <button className='success'>
+              create tag
+            </button>
+        </div>
+      </div>
     </>
   );
 };
