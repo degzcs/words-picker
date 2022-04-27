182333670c39a618b3f367f77b565b4b8d6b1655
Diego Gomez <diego.gomez@encora.com>
2022-04-27 10:44:04 -0500

    add tag words funtionality

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,7 +3,9 @@ import Routes from "../routes/Index";
 
 const App = () => {
   const [sentence, setSentence] = useState('');
+  const [tag, setTag] = useState('');
   const [selectedWords, setSelectedWords] = useState([]);
+  const [taggedWords, setTaggedWords] = useState({});
 
   useEffect(() => {
     setSentence('Apple is looking at buying U.K. startup for $1 billion.')
@@ -11,6 +13,20 @@ const App = () => {
 
   const handleClick = (word) => {
     setSelectedWords([...selectedWords, word]);
+  }
+
+  const tagWords = (words, tag) => {
+    setTaggedWords({...taggedWords, [tag]: words});
+    setTag('');
+    setSelectedWords([]);
+  }
+
+  const onChangeTag = (e) => {
+    setTag(e.target.value);
+  };
+
+  const highlightWords = () => {
+    // TODO select tagged words and update their style
   }
 
   return (
@@ -37,11 +53,22 @@ const App = () => {
                 </span>
               })
             }
-          <input type='text'/>
+            <div className="form-group pt-5">
+              <label htmlFor="tag">Tag</label>
+              <input
+                type="text"
+                name="tag"
+                id="tag"
+                className="form-control"
+                value={tag}
+                required
+                onChange={(e) => onChangeTag(e)}
+              />
+            </div>
         </div>
         <div
           className="success">
-            <button className='success'>
+            <button className='success' onClick={() => tagWords(selectedWords, tag)}>
               create tag
             </button>
         </div>
